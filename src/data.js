7397e5b2f64c3385fcadf2b3f30d3c74f660f3f5
moween <moween1998@gmail.com>
2021-06-18 15:52:52 +0100

    Add data

---
+++ src/data.js
@@ -0,0 +1,89 @@
+const data = [
+  {
+    adult: false,
+    backdrop_path: "/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg",
+    genre_ids:  [28, 12, 14, 878],
+    id: 791373,
+    original_language: "en",
+    original_title: "Zack Snyder's Justice League",
+    overview: "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
+    popularity: 733.262,
+    poster_path: "https://image.tmdb.org/t/p/w500//tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg",
+    release_date: "2021-03-18",
+    title: "Zack Snyder's Justice League",
+    video: false,
+    vote_average: 8.5,
+    vote_count: 5792
+  },
+
+  { 
+    adult: false,
+    backdrop_path: "/fPGeS6jgdLovQAKunNHX8l0avCy.jpg",
+    genre_ids: [28, 53, 10752],
+    id: 567189,
+    original_language: "en",
+    original_title: "Tom Clancy's Without Remorse",
+    overview: "An elite Navy SEAL uncovers an international conspiracy while seeking justice for the murder of his pregnant wife.",
+    popularity: 1130.112,
+    poster_path: "https://image.tmdb.org/t/p/w500//rEm96ib0sPiZBADNKBHKBv5bve9.jpg",
+    release_date: "2021-04-29",
+    title: "Tom Clancy's Without Remorse",
+    video: false,
+    vote_average: 7.2,
+    vote_count: 1177
+
+  },
+
+  {
+    adult: false,
+    backdrop_path: "/jw6ASGRT2gi8EjCImpGtbiJ9NQ9.jpg",
+    genre_ids: [ 27 ],
+    id: 632357,
+    original_language: "en",
+    original_title: "The Unholy",
+    overview: "Alice, a young hearing-impaired girl who, after a supposed visitation from the Virgin Mary, is inexplicably able to hear, speak and heal the sick. As word spreads and people from near and far flock to witness her miracles, a disgraced journalist hoping to revive his career visits the small New England town to investigate. When terrifying events begin to happen all around, he starts to question if these phenomena are the works of the Virgin Mary or something much more sinister.",
+    popularity: 1026.613,
+    poster_path: "https://image.tmdb.org/t/p/w500//bShgiEQoPnWdw4LBrYT5u18JF34.jpg",
+    release_date: "2021-03-31",
+    title: "The Unholy",
+    video: false,
+    vote_average: 7,
+    vote_count: 962
+  },
+
+  {
+    adult: false,
+    backdrop_path: "/ouOojiypBE6CD1aqcHPVq7cJf2R.jpg",
+    genre_ids: [53, 18, 28, 9648],
+    id: 578701,
+    original_language: "en",
+    original_title: "Those Who Wish Me Dead",
+    overview: "A young boy finds himself pursued by two assassins in the Montana wilderness with a survival expert determined to protecting him - and a forest fire threatening to consume them all.",
+    popularity: 921.595,
+    poster_path: "https://image.tmdb.org/t/p/w500//xbSuFiJbbBWCkyCCKIMfuDCA4yV.jpg",
+    release_date: "2021-05-05",
+    title: "Those Who Wish Me Dead",
+    video: false,
+    vote_average: 7,
+    vote_count: 500
+  },
+
+  {
+    adult: false,
+    backdrop_path: "/wjQXZTlFM3PVEUmKf1sUajjygqT.jpg",
+    genre_ids: [878, 28, 53],
+    id: 581726,
+    original_language: "en",
+    original_title: "Infinite",
+    overview: "Evan McCauley has skills he never learned and memories of places he has never visited. Self-medicated and on the brink of a mental breakdown, a secret group that call themselves “Infinites” come to his rescue, revealing that his memories are real.",
+    popularity: 855.476,
+    poster_path: "https://image.tmdb.org/t/p/w500//niw2AKHz6XmwiRMLWaoyAOAti0G.jpg",
+    release_date: "2021-09-08",
+    title: "Infinite",
+    video: false,
+    vote_average: 0,
+    vote_count: 0
+  }
+];
+
+export default data;
